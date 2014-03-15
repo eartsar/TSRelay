763a2fc6ffc663f527be4cb7dbd8a4533432dcf1
Fura Barumaru <ear7631@gmail.com>
2014-03-15 17:28:19 -0400

    Removed unused dir, added ground gui window

--- TSRelay/TSRelayPlugin.cs
+++ TSRelay/TSRelayPlugin.cs
@@ -75,11 +75,37 @@ namespace ACT_Plugin
             InitializeComponent();
         }
 
-        string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\PluginSample.config.xml");
+        string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\TSRelay.config.xml");
+
+
+        TreeNode optionsNode = null;
 
         #region IActPluginV1 Members
         public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
         {
+            // This is the GUI related stuff
+            pluginScreenSpace.Controls.Add(this);
+            this.Dock = DockStyle.Fill;
+
+            int dcIndex = -1;   // Find the Data Correction node in the Options tab
+            for (int i = 0; i < ActGlobals.oFormActMain.OptionsTreeView.Nodes.Count; i++)
+            {
+                if (ActGlobals.oFormActMain.OptionsTreeView.Nodes[i].Text == "Data Correction")
+                    dcIndex = i;
+            }
+            if (dcIndex != -1)
+            {
+                // Add our own node to the Data Correction node
+                optionsNode = ActGlobals.oFormActMain.OptionsTreeView.Nodes[dcIndex].Nodes.Add("EQ2 English Settings");
+                // Register our user control(this) to our newly create node path.  All controls added to the list will be laid out left to right, top to bottom
+                ActGlobals.oFormActMain.OptionsControlSets.Add(@"Data Correction\EQ2 English Settings", new List<Control> { this });
+                Label lblConfig = new Label();
+                lblConfig.AutoSize = true;
+                lblConfig.Text = "Find the applicable options in the Options tab, Data Correction section.";
+                pluginScreenSpace.Controls.Add(lblConfig);  // This is a placeholder label for when the UserControl is removed from the plugins tab and shown in the Options tab
+            }
+
+
             // Register the log read event
             ActGlobals.oFormActMain.OnLogLineRead += new LogLineEventDelegate(oFormActMain_LogLineEvent);
 
